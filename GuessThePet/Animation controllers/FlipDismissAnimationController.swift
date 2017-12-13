5f8d73f3b0014be48f460108b490a54355441953
Tiago Do Couto <cs.couto@outlook.com>
2017-12-13 13:13:45 -0600

    [cardviewcontroller] - dismissing animations

--- GuessThePet/Animation controllers/FlipDismissAnimationController.swift
+++ GuessThePet/Animation controllers/FlipDismissAnimationController.swift
@@ -28,7 +28,14 @@
 
 import UIKit
 
-class FlipPresentAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
+class FlipDismissAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
+  
+  private let destinationFrame: CGRect
+  
+  init(destinationFrame: CGRect) {
+    self.destinationFrame = destinationFrame
+  }
+  
   func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
     return 0.6
   }
@@ -37,65 +44,51 @@ class FlipPresentAnimationController: NSObject, UIViewControllerAnimatedTransiti
     // 1
     guard let fromVC = transitionContext.viewController(forKey: .from),
       let toVC = transitionContext.viewController(forKey: .to),
-      let snapshot = toVC.view.snapshotView(afterScreenUpdates: true)
+      let snapshot = fromVC.view.snapshotView(afterScreenUpdates: false)
       else {
         return
     }
     
-    // 2
-    let containerView = transitionContext.containerView
-    let finalFrame = transitionContext.finalFrame(for: toVC)
-    
-    // 3
-    snapshot.frame = originFrame
     snapshot.layer.cornerRadius = CardViewController.cardCornerRadius
     snapshot.layer.masksToBounds = true
     
-    // 1
-    containerView.addSubview(toVC.view)
-    containerView.addSubview(snapshot)
-    toVC.view.isHidden = true
-    
     // 2
-    AnimationHelper.perspectiveTransform(for: containerView)
-    snapshot.layer.transform = AnimationHelper.yRotation(.pi / 2)
+    let containerView = transitionContext.containerView
+    containerView.insertSubview(toVC.view, at: 0)
+    containerView.addSubview(snapshot)
+    fromVC.view.isHidden = true
+    
     // 3
+    AnimationHelper.perspectiveTransform(for: containerView)
+    toVC.view.layer.transform = AnimationHelper.yRotation(-.pi / 2)
     let duration = transitionDuration(using: transitionContext)
     
-    // 1
     UIView.animateKeyframes(
       withDuration: duration,
       delay: 0,
       options: .calculationModeCubic,
       animations: {
-        // 2
+        // 1
         UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/3) {
-          fromVC.view.layer.transform = AnimationHelper.yRotation(-.pi / 2)
+          snapshot.frame = self.destinationFrame
         }
         
-        // 3
         UIView.addKeyframe(withRelativeStartTime: 1/3, relativeDuration: 1/3) {
-          snapshot.layer.transform = AnimationHelper.yRotation(0.0)
+          snapshot.layer.transform = AnimationHelper.yRotation(.pi / 2)
         }
         
-        // 4
         UIView.addKeyframe(withRelativeStartTime: 2/3, relativeDuration: 1/3) {
-          snapshot.frame = finalFrame
-          snapshot.layer.cornerRadius = 0
+          toVC.view.layer.transform = AnimationHelper.yRotation(0.0)
         }
     },
-      // 5
+      // 2
       completion: { _ in
-        toVC.view.isHidden = false
+        fromVC.view.isHidden = false
         snapshot.removeFromSuperview()
-        fromVC.view.layer.transform = CATransform3DIdentity
+        if transitionContext.transitionWasCancelled {
+          toVC.view.removeFromSuperview()
+        }
         transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
     })
-  }
-  
-  private let originFrame: CGRect
-  
-  init(originFrame: CGRect) {
-    self.originFrame = originFrame
   }
 }
